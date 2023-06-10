17a60d388044cbe2709123b2442a0da8f38dd377
Darley Peralta <ordeal00s@gmail.com>
2023-06-10 17:02:10 -0400

    1st

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,20 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>To Do List</h1>
+        <ul>
+          <li>Walk Dawg</li>
+          <li>Take Out Trash</li>
+          <li>Work Out</li>
+        </ul>
+
+       <form>
+        <input type="text"/>
+        <button>Add</button>
+        </form> 
+
+       <button>Clear</button> 
+
       </div>
     )
   }
