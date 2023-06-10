a561ee3d213a846fd63ef93b5b65eb09a6c09092
Darley Peralta <ordeal00s@gmail.com>
2023-06-10 17:28:33 -0400

    built state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,22 +1,44 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: true
+        }
+      ]
+    }
+  }
+
   render() {
+    const { todos } = this.state;
+    console.log(todos)
     return (
       <div>
         <h1>To Do List</h1>
         <ul>
-          <li>Walk Dawg</li>
-          <li>Take Out Trash</li>
-          <li>Work Out</li>
+          {
+            todos.map( todo=>{
+              return (<li>{todo.task} {todo.completed? <span>--Completed--</span>:<span></span>}</li>)
+            })
+          }
         </ul>
 
-       <form>
-        <input type="text"/>
-        <button>Add</button>
-        </form> 
+        <form>
+          <input type="text" />
+          <button>Add</button>
+        </form>
 
-       <button>Clear</button> 
+        <button>Clear</button>
 
       </div>
     )
